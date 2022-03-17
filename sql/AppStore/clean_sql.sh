88d0a738551f9b93563ae659fcbb9ed8b9bd3d40
Muhammed Anas <anas.m6@gmail.com>
2022-03-18 02:40:06 +0800

    Edited SQL scripts

---
+++ sql/AppStore/clean_sql.sh
@@ -0,0 +1,1 @@
+psql $DATABASE_URL -f AppStoreClean.sql
--- sql/clean.sql
+++ sql/clean.sql
@@ -4,9 +4,9 @@
 
 *******************/
 
-DROP TABLE IF EXISTS modules;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS administrators;
-DROP TABLE IF EXISTS tutors;
-DROP TABLE IF EXISTS tutees;
 DROP TABLE IF EXISTS offers;
+DROP TABLE IF EXISTS tutees;
+DROP TABLE IF EXISTS tutors;
+DROP VIEW IF EXISTS administrators;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS modules;
