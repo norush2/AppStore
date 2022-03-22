09ffe78be8f1ff759a3cbd9d3a609f9ff22b0af2
norush2 <101162759+norush2@users.noreply.github.com>
2022-03-22 16:33:27 +0800

    Update AppStoreClean.sql

--- sql/AppStoreClean.sql
+++ sql/AppStoreClean.sql
@@ -4,8 +4,9 @@
 
 *******************/
 
-DROP TABLE IF EXISTS modules;
 DROP TABLE IF EXISTS offers;
 DROP TABLE IF EXISTS tutees;
 DROP TABLE IF EXISTS tutors;
+DROP VIEW IF EXISTS administrators;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS modules;
